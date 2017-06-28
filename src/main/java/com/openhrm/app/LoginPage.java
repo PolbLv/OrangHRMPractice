e7e9ae8ff88ae071eddd7f7ef0fbdb654b22d397
LVIVSOFT\spolyakov <--s.polb.lv@gmail.com>
2017-06-28 14:44:09 +0300

    Some Changes

--- src/main/java/com/openhrm/app/LoginPage.java
+++ src/main/java/com/openhrm/app/LoginPage.java
@@ -1,11 +1,22 @@
 package com.openhrm.app;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by LVIVSOFT\spolyakov on 28.06.17.
  */
 public class LoginPage extends AbstractPage {
+
+    @FindBy(id = "txtUsername")
+    private WebElement userNameInput;
+
+    @FindBy(id = "txtPassword")
+    private WebElement passwordInput;
+
+    @FindBy(id = "btnLogin")
+    private WebElement clickBurronLogin;
 
 
     public LoginPage(WebDriver driver){
